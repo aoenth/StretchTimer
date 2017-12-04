0fe1ec0f169f9403424345aa8e33e94c17180877
Kevin <Kevin@Kevins-MacBook-Pro.local>
2017-12-03 23:59:21 -0500

    Initial Commit

---
+++ StretchTimer/BreakTimer.swift
@@ -0,0 +1,79 @@
+//
+//  BreakTimer.swift
+//  Disciplines
+//
+//  Created by Kevin on 2017-09-21.
+//  Copyright © 2017 Monorail Apps. All rights reserved.
+//
+
+import UIKit
+import AudioToolbox
+
+class BreakTimer: UIView {
+    var counter = 0.0
+    var timer: Timer?
+    var isTimerOn = false
+    var isDone = false
+    var timeInterval: Double!
+    let HEIGHT = UIScreen.main.bounds.height
+    let WIDTH = UIScreen.main.bounds.width
+    var delegate: WhatIsNextDelegate? = nil
+    var COUNTDOWN = Double(1*60)
+    var progressView: UIView!
+    var timeLbl: UILabel!
+      
+    override init(frame: CGRect) {
+        super.init(frame: CGRect(x: 0, y: 0, width: WIDTH, height: HEIGHT))
+        
+        progressView = UIView(frame: CGRect(x: 0, y: 0, width: WIDTH, height: HEIGHT))
+        progressView.backgroundColor = UIColor.orange
+        self.backgroundColor = UIColor.white
+        timeInterval = COUNTDOWN/Double(HEIGHT)
+        timeLbl = UILabel(frame: CGRect(x: WIDTH/2-150, y: HEIGHT/2-100, width: 300, height: 200))
+        timeLbl.font = UIFont(name: "AvenirNextCondensed-UltraLight", size: 120)
+        timeLbl.textAlignment = .center
+        timeLbl.adjustsFontSizeToFitWidth = false
+        
+    }
+    
+
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    func playPause() {
+        if isTimerOn {
+            timer?.invalidate()
+            isTimerOn = false
+        } else {
+            self.addSubview(progressView)
+            timer = Timer.scheduledTimer(timeInterval: timeInterval, target: self, selector: #selector(updateHeight), userInfo: nil, repeats: true)
+            isTimerOn = true
+        }
+    }
+
+
+    func updateHeight() {
+        let updatedValue = (COUNTDOWN - counter) / COUNTDOWN * Double(HEIGHT)
+        progressView.frame.origin.y = CGFloat(updatedValue)
+        if counter >= COUNTDOWN {
+            timer?.invalidate()
+            AudioServicesPlayAlertSound(SystemSoundID(kSystemSoundID_Vibrate))
+            delegate?.endActions()
+            return
+        } else {
+            counter += timeInterval
+            timeLbl.text = String(format: "%.2f", counter)
+            if updatedValue < Double(HEIGHT/2) {
+                timeLbl.textColor = UIColor.white
+            }
+            self.addSubview(timeLbl)
+        }
+    }
+    
+}
+
+protocol WhatIsNextDelegate {
+    func endActions()
+}
--- StretchTimer/ViewController.swift
+++ StretchTimer/ViewController.swift
@@ -9,17 +9,80 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let counter = BreakTimer(frame: CGRect.zero)
+    var startButton: UIButton!
+    var startButtonLbl: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        UIApplication.shared.isIdleTimerDisabled = true
+        
+        let screenSize = UIScreen.main.bounds
+        let buttonWidth = screenSize.width
+        let buttonHeight = screenSize.height
+        let buttonFrame = CGRect(x: (screenSize.width - buttonWidth)/2, y: (screenSize.height - buttonHeight)/2, width: buttonWidth, height: buttonHeight)
+        startButton = UIButton(frame: buttonFrame)
+        startButton.backgroundColor = UIColor.purple
+        view.addSubview(startButton)
+        startButtonLbl = UILabel(frame: CGRect(x: (screenSize.width - buttonWidth*0.8)/2, y: (screenSize.height - buttonHeight)/2, width: buttonWidth*0.8 , height: buttonHeight))
+        startButtonLbl.font = startButtonLbl.font.withSize(CGFloat(buttonHeight * 0.618))
+        let text = NSMutableAttributedString(string: "Start Stretching", attributes: [NSFontAttributeName: UIFont(name: "AvenirNextCondensed-UltraLight", size: 90)!])
+        text.addAttribute(NSForegroundColorAttributeName, value: UIColor.white,
+                          range: NSRange(location: 0, length: "Start Stretching".characters.count))
+        startButtonLbl.attributedText = text
+        startButtonLbl.adjustsFontSizeToFitWidth = true
+        startButtonLbl.textAlignment = .natural
+        view.addSubview(startButtonLbl)
+        startButton.addTarget(self, action: #selector(startStretching), for: .touchUpInside)
+        counter.delegate = self
+        let g = UISwipeGestureRecognizer(target: self, action: #selector(stopStretching(_ :)))
+        self.view.addGestureRecognizer(g)
+        g.delaysTouchesBegan = true
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        startStretching()
     }
-
+    
+    func stopStretching(_ gestureRecognizer: UIGestureRecognizer) {
+        if gestureRecognizer.state == .ended {
+            if counter.isTimerOn {
+                counter.playPause()
+            }
+            reset()
+        }
+    }
+    
+    func startStretching() {
+        if counter.counter == 0 || counter.isDone {
+            counter.counter = 0
+            view.addSubview(counter)
+            counter.isDone = false
+            counter.backgroundColor = UIColor.white
+        }
+        counter.playPause()
+    }
+    
+    func reset() {
+        counter.backgroundColor = UIColor.clear
+        counter.isDone = true
+        counter.isTimerOn = false
+        counter.progressView.removeFromSuperview()
+        counter.timeLbl.text = ""
+        counter.counter = 0
+        counter.timeLbl.textColor = UIColor.black
+    }
 
 }
+
+extension ViewController: WhatIsNextDelegate {
+    func endActions() {
+        reset()
+    }
+}
+
+
 
